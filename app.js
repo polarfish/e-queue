340433ce48398abac28a61b5fb08cd13ee93ead0
Eugene Grybinnyk <eugene.grybinnyk@gmail.com>
2013-07-14 01:17:18 +0300

    super fix

--- app.js
+++ app.js
@@ -1,64 +1,141 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var crypto = require('crypto')
+    , md5sum = crypto.createHash('md5')
+    , express = require('express')
+    , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
 
 // Routes
 app.get('/', routes.index);
+
+// ADMIN ROUTES
+
 app.post('/admin/create_queue/:queueName', function (req, res) {
-    var header = req.headers['authorization'] || '',        // get the header
-        token = header.split(/\s+/).pop() || '',            // and the encoded auth token
-        auth = new Buffer(token, 'base64').toString(),    // convert from base64
-        parts = auth.split(/:/),                          // split on colon
+    var header = req.headers['authorization'] || '',
+        token = header.split(/\s+/).pop() || '',
+        auth = new Buffer(token, 'base64').toString(),
+        parts = auth.split(/:/),
         username = parts[0],
         password = parts[1];
 
     console.log("Authentication: username : '" + username + "', password : '" + password + "'");
 
-    User.findOne({"name":username}, function(err, user) {
+    User.findOne({"name": username}, function (err, user) {
         if (user && (user.password == password)) {
             console.log("Authorized!");
             (new Queue({
                 name: req.params.queueName,
-                startDate: '10-10-10',
+                startDate: new Date(0),
                 userId: user.id,
-                ticketsGiver: 0,
+                ticketsGiven: 0,
                 currentTicketNumber: 0,
-                isActive: true
-            })).save(function (err, doc) {
+                isActive: false
+            })).save(function (err, queue) {
                     if (err) {
                         console.log(err);
+                        res.send(err, 500);
+                    } else {
+                        console.log('Queue added: {"name": ' + queue.name + ', "user":' + user.name + '} ');
+                        res.send(queue);
                     }
-                    console.log('Queue added: {"name": ' + req.params.queueName + ', "user":} ' + doc.name);
-                    res.send(doc);
-            })
-            return;
+                })
+        } else {
+            res.send(403);
         }
-        res.send("Error 403!");
+    });
+});
+
+app.put('/admin/activate_queue/:queueName', function (req, res) {
+    var header = req.headers['authorization'] || '',
+        token = header.split(/\s+/).pop() || '',
+        auth = new Buffer(token, 'base64').toString(),
+        parts = auth.split(/:/),
+        username = parts[0],
+        password = parts[1];
+
+    console.log("Authentication: username : '" + username + "', password : '" + password + "'");
+
+    User.findOne({"name": username}, function (err, user) {
+        if (user && (user.password == password)) {
+            console.log("Authorized!");
+            Queue.update({name: req.params.queueName}, {$set: {isActive: true, startDate: new Date()}}, function(err, num, raw) {
+                if (err) {
+                    console.log(err);
+                    res.send(err, 500);
+                } else {
+                    if (raw.n > 0) {
+                        console.log("Queue " + req.params.queueName + " activated");
+                    }
+                    res.send(raw, 200);
+                }
+            });
+        } else {
+            res.send(403);
+        }
+    });
+});
+
+app.put('/admin/deactivate_queue/:queueName', function (req, res) {
+    var header = req.headers['authorization'] || '',
+        token = header.split(/\s+/).pop() || '',
+        auth = new Buffer(token, 'base64').toString(),
+        parts = auth.split(/:/),
+        username = parts[0],
+        password = parts[1];
+
+    console.log("Authentication: username : '" + username + "', password : '" + password + "'");
+
+    User.findOne({"name": username}, function (err, user) {
+        if (user && (user.password == password)) {
+            console.log("Authorized!");
+            Queue.update({name: req.params.queueName}, {$set: {isActive: false, startDate:new Date(0), ticketsGiven: 0}}, function(err, num, raw) {
+                if (err) {
+                    console.log(err);
+                    res.send(err, 500);
+                } else {
+                    if (raw.n > 0) {
+                        Queue.findOne({name: req.params.queueName}, function (err, queue) {
+                            if (err) {
+                                console.log(err);
+                                res.send(err, 500);
+                            } else {
+                                Ticket.remove({queueId: queue.id}, function(err, ticketsRemoved) {
+                                    console.log("Queue " + queue.name + " deactivated. " + ticketsRemoved + " tickets removed." );
+                                    res.send(200);
+                                });
+                            }
+                        })
+                    } else {
+                        res.send("Undefined queue" + req.params.queueName, 404);
+                    }
+                }
+            });
+        } else {
+            res.send(403);
+        }
     });
 });
 
@@ -67,18 +144,7 @@ app.get('/admin/:userId/queues', function (req, res) {
         res.send(queues);
     })
 });
-app.get('/admin/:queueId/ticket', function (req, res) {
-    Queue.findById(req.params['queueId'], function (err, doc) {
-        (new Ticket({
-            queueId: doc.id,
-            number: doc.ticketsGiver + 1,
-            hash: 'asdfasdfasdfa',
-            isActive: true
-        })).save(function (err, doc) {
-            res.send(doc);
-        })
-    })
-});
+
 app.post('/admin/:number/ticket', function (req, res) {
     Ticket.update({number: req.params['number']}, {isActive: false}, function (err, affected, raw) {
         if (err) {
@@ -90,82 +156,139 @@ app.post('/admin/:number/ticket', function (req, res) {
 });
 
 
+// USER ROUTES
+
+app.get('/user/:userName/:queueName/info', function (req, res) {
+
+    User.findOne({"name": req.params.userName}, function (err, user) {
+        if (user) {
+            Queue.findOne({"name":req.params.queueName}, function (err, queue) {
+                if (queue) {
+                    res.send(queue, 200);
+                } else {
+                    res.send("Undefined queue " + req.params.queueName, 404);
+                }
+            });
+        } else {
+            res.send("Undefined user " + req.params.userName, 404);
+        }
+    });
+});
+
+app.get('/user/:userName/:queueName/get_ticket', function (req, res) {
+    User.findOne({"name": req.params.userName}, function (err, user) {
+        if (user) {
+            Queue.findOne({name: req.params.queueName}, function(err, queue) {
+                if (err) {
+                    console.log(err);
+                    res.send(err, 500);
+                } else {
+                    if (queue) {
+                        (new Ticket({
+                            queueId: queue.id,
+                            number: queue.ticketsGiven + 1,
+                            hash: Math.random(),
+                            isProcessed: false
+                        })).save(function (err, ticket) {
+                                if (err) {
+                                    console.log(err);
+                                    res.send(err, 500);
+                                } else {
+                                    queue.ticketsGiven++;
+                                    queue.save(function(err, queue) {
+                                        if (err) {
+                                            console.log(err);
+                                            res.send(err, 500);
+                                        } else {
+                                            res.send(ticket, 200);
+                                        }
+                                    });
+                                }
+                            })
+                    } else {
+                        res.send("Undefined queue " + req.params.queueName, 404);
+                    }
+                }
+            });
+        } else {
+            res.send("Undefined user " + req.params.userName, 404);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Connect to mongo DB
-var mongoose = require("mongoose"),
+var Mongoose = require("mongoose"),
     queueDB;
 
 /* DATABASE SCHEMAS */
-var userSchema = mongoose.Schema({
+var userSchema = Mongoose.Schema({
         name: String,
         password: String
     }),
-    queueSchema = mongoose.Schema({
+    queueSchema = Mongoose.Schema({
         name: String,
         startDate: String,
         userId: String,
-        ticketsGiver: Number,
+        ticketsGiven: Number,
         currentTicketNumber: Number,
         isActive: Boolean
     }),
-    ticketSchema = mongoose.Schema({
+    ticketSchema = Mongoose.Schema({
         queueId: String,
         number: String,
         hash: String,
-        isActive: Boolean
+        isProcessed: Boolean
     })
 /* END DATABASE SCHEMAS */
 
 /* DATABASE MODELS */
-    User = mongoose.model('User', userSchema),
-    Queue = mongoose.model('Queue', queueSchema),
-    Ticket = mongoose.model('Ticket', ticketSchema);
+User = Mongoose.model('User', userSchema),
+    Queue = Mongoose.model('Queue', queueSchema),
+    Ticket = Mongoose.model('Ticket', ticketSchema);
 
 /* END DATABASE MODELS */
 
-mongoose.connect('mongodb://localhost/queue');
-queueDB = mongoose.connection;
+Mongoose.connect('mongodb://localhost/queue');
+queueDB = Mongoose.connection;
 
 //Error handling if conncetion fails
 queueDB.on('error', console.error.bind(console, 'connection error:'));
 //Check if successful connection is made
-queueDB.once('open', function callback () {
+queueDB.once('open', function callback() {
     console.log("Connection success.");
-
-
-
-        console.log('Initializing DB:')
-        User.remove({}, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log('Database has been dropped...');
-            (new User({
-                name: 'donetsk_post',
-                password: 'donetsk_post'
-            })).save(function (error, doc) {
+    console.log('Initializing DB:')
+    User.remove({}, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Database has been dropped...');
+        (new User({
+            name: 'donetsk_post',
+            password: 'donetsk_post'
+        })).save(function (error, doc) {
                 if (error) {
                     console.log(error);
                 }
                 console.log('Add test user ' + doc.name + ' with password ' + doc.password);
             });
-            (new Queue({
-                name: 'test',
-                startDate: '10-10-10',
-                userId: '123123',
-                ticketsGiver: 1,
-                isActive: true,
-                currentTicketNumber: 1
-            })).save(function (err, doc) {
-                if (err) {
-                    console.log(err);
-                }
-                console.log('Add test queue ' + doc.name);
-            })
-        });
+    });
 });
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(3000, function () {
+    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
